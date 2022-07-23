5e5dd703d3afdfedb049c07feb8d020422582a7a
Alex Krol <krolmail@list.ru>
2022-07-23 16:57:11 +0300

    Fix LifeCycle example

--- src/components/LifeCycle/LifeCycle.jsx
+++ src/components/LifeCycle/LifeCycle.jsx
@@ -2,7 +2,62 @@ import React from 'react';
 import style from './LifeCycle.module.css';
 
 export class LifeCycle extends React.Component {
+  state = {
+    field: 0
+  };
+
+  handler = () => {
+    this.setState(state => ({field: state.field + 1}));
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('getDerivedStateFromProps');
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    // setInterval(() => { console.log('!') }, 5000);
+    // document.addEventListener('scroll', this.handler);
+
+    // eslint-disable-next-line react/prop-types
+    // document.title = this.props.prop;
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    console.log('shouldComponentUpdate');
+
+    // return this.state !== nextState || this.props !== nextProps;
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(pervProps, pervState) {
+    console.log('getSnapshotBeforeUpdate');
+    return window.pageYOffset;
+  }
+
+  componentDidUpdate(pervProps, pervState, snapshot) {
+    console.log('componentDidUpdate');
+    // window.scrollBy(0, -snapshot);
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+    // document.removeEventListener('scroll', this.handler)
+  }
+
+  static getDerivedStateFromError(err) {
+    return {
+      hasError: true
+    };
+  }
+
+  componentDidCatch(err, errInfo) {
+    // sendLog(errInfo.componentStack);
+  }
+
   render() {
+    console.log('render');
     return (
       <div>
         <h1 className={style.title}>Жизненный цикл</h1>
@@ -27,6 +82,11 @@ export class LifeCycle extends React.Component {
             </ul>
           </div>
         </div>
+
+        <button className={style.btn}
+          onClick={this.handler}>
+          Клик {this.state.field}
+        </button>
       </div>
     );
   }
